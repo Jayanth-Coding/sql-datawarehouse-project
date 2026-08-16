295c3b17b3fcd9c080c67cfa234a8f2e5c5e8c27
Jayanth-Coding <kollujayanth@gmail.com>
2026-08-17 00:58:06 +0530

    Update bulk_insert_from_csv-03.sql

--- scripts/bulk_insert_from_csv-03.sql
+++ scripts/bulk_insert_from_csv-03.sql
@@ -1,68 +1,77 @@
-USE DataWareHouse;
-GO
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
 
-TRUNCATE TABLE bronze.crm_cust_info;
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_crm\cust_info.csv'
-WITH
-(
-FIRSTROW = 2,
-FIELDTERMINATOR = ',',
-TABLOCK
-);
-GO
+	PRINT '--------------------'
+	PRINT 'Loading Bronze Layer'
+	PRINT '--------------------'
+	PRINT 'Loading CRM Tables'
+	PRINT '--------------------'
+	TRUNCATE TABLE bronze.crm_cust_info;
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_crm\cust_info.csv'
+	WITH
+	(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+	);
 
-TRUNCATE TABLE bronze.crm_prd_info;
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_crm\prd_info.csv'
-WITH
-(
-FIRSTROW = 2,
-FIELDTERMINATOR = ',',
-TABLOCK
-);
-GO
 
-TRUNCATE TABLE bronze.crm_sales_details;
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_crm\sales_details.csv'
-WITH
-(
-FIRSTROW = 2,
-FIELDTERMINATOR = ',',
-TABLOCK
-);
-GO
+	TRUNCATE TABLE bronze.crm_prd_info;
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_crm\prd_info.csv'
+	WITH
+	(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+	);
 
-TRUNCATE TABLE bronze.erp_cust_az12;
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_erp\CUST_AZ12.csv'
-WITH
-(
-FIRSTROW = 2,
-FIELDTERMINATOR = ',',
-TABLOCK
-);
-GO
 
-TRUNCATE TABLE bronze.erp_loc_a101;
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_erp\LOC_A101.csv'
-WITH
-(
-FIRSTROW = 2,
-FIELDTERMINATOR = ',',
-TABLOCK
-);
-GO
+	TRUNCATE TABLE bronze.crm_sales_details;
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_crm\sales_details.csv'
+	WITH
+	(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+	);
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH
-(
-FIRSTROW = 2,
-FIELDTERMINATOR = ',',
-TABLOCK
-);
-GO
+	PRINT '--------------------'
+	PRINT 'Loading ERP Tables'
+	PRINT '--------------------'
+	TRUNCATE TABLE bronze.erp_cust_az12;
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	WITH
+	(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+	);
+
+
+	TRUNCATE TABLE bronze.erp_loc_a101;
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_erp\LOC_A101.csv'
+	WITH
+	(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+	);
+
+
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\jayan\Documents\sql-datawarehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH
+	(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+	);
+
+
+END;
