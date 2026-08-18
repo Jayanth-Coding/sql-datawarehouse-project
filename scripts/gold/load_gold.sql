283a5c64e0e488eed06da6f4ceb1dd4a51655a88
Jayanth-Coding <kollujayanth@gmail.com>
2026-08-18 13:35:02 +0530

    Update load_gold.sql

--- scripts/gold/load_gold.sql
+++ scripts/gold/load_gold.sql
@@ -35,3 +35,20 @@ from silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
 ON pn.cat_id = pc.id
 WHERE prd_end_dt IS NULL;
+
+CREATE VIEW GOLD.FACT_SALES AS
+SELECT
+sd.sls_ord_num as order_number,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt as order_date,
+sd.sls_ship_dt as shipping_date,
+sd.sls_due_dt as due_date,
+sd.sls_sales as sales_amount,
+sd.sls_quantity as quantity,
+sd.sls_price as price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id;
